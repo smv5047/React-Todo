fff2b8a9fc7b9b1079d885800855ac35164aae95
smv5047 <smv5047@gmail.com>
2019-10-02 21:36:41 -0400

    Laid Out infrastructure for ADding Todos, Todo input bar rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
+
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      todo: []
+    }
+  }
+
+
+  addTodo = (event, todoName) => {
+    event.preventDefault();
+
+    const newTodo = {
+
+      id: Date.now(),
+      task: todoName,
+      completed: false
+    }
+
+    this.setState({
+      todo:[...this.state.todo, newTodo]
+    })
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+          <h2>ToDo Today</h2>
+          <TodoForm addTodo={this.addTodo} />
+          <TodoList todoArray={this.state.todo}/>
+
+          {/* <button onClick={this.clearDone}>
+            Done
+          </button> */}
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,26 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from "react";
+import Todo from "./Todo";
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      todo: props.todoArray
+    };
+  }
+
+  render() {
+    return (
+      <>
+        {this.props.todoArray.map(item => (
+          <Todo key={item.id} item={item} />
+        ))}
+      </>
+    );
+  }
+}
+
+export default TodoList;
